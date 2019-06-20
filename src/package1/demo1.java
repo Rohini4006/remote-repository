0b24f0f364a3234961d7079913c56c0f218e9c93
Rohini Rautray <rohini.rautray@ad.infosys.com>
2019-06-20 12:31:30 +0530

    Selenium Project written Signed-off-by: Rohini Rautray <rohini.rautray@ad.infosys.com>

--- src/package1/demo1.java
+++ src/package1/demo1.java
@@ -1,9 +1,19 @@
 package package1;
 
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class demo1 {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Java");
+		System.setProperty("webdriver.chrome.driver", "D:\\POC using ITAW\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		String baseurl = "https://www.guru99.com";
+		driver.get(baseurl);
+		String pageTitle = driver.getTitle();
+		System.out.println("Title of the page is "+ pageTitle);
+		driver.close();
 	}
 
 }
